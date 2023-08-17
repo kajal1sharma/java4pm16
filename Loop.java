a087b0f5d422704c46483fed4f3db9dd20251d8f
kajal <kajal@gmail.com>
2023-08-17 16:54:52 +0530

    ..

--- Loop.java
+++ Loop.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
 public class Loop {
     public static void main(String[] args) {
        
-        for(int i=0;i<10;i=i+1){
-            System.out.println("Hello world");
-            int a=90;
-            int b=80;
-            int c=a+b;
-            System.out.println(c);
+        Scanner sc = new Scanner(System.in);
+
+
+        // initialization;condition;updation
+        for(int i=0;i<5;i=i+1){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int sum = a+b;
+            System.out.println(sum);
         }
-        //Q => print even number 1-100
+
+        
+
+
+        // int a;
+        // a= sc.nextInt();
+
+        // System.out.println(a);
+
+        // boolean a= sc.nextBoolean();
+        // System.out.println(a);
+        // float f= sc.nextFloat();
+        // System.out.println(f);
+        // String str= sc.next();
+        // System.out.println(str);
+        // String str2 = sc.nextLine();
+        // System.out.println(str2);
 
     }
 }
