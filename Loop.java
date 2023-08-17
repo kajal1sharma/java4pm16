534e2eb8b6f26cd60d83ee6f16e4274bfac0b38a
kajal <kajal@gmail.com>
2023-08-17 17:36:07 +0530

    ..

--- Loop.java
+++ Loop.java
@@ -6,14 +6,48 @@ public class Loop {
 
 
         // initialization;condition;updation
-        for(int i=0;i<5;i=i+1){
-            System.out.println("enter the first number");
-            int a=sc.nextInt();
-            System.out.println("Enter the second number");
-            int b=sc.nextInt();
-            int sum = a+b;
-            System.out.println("the sum is : " +sum);
-        }
+        //infinite
+        // for(;;){
+        //     System.out.println("Hello world");
+        // }
+        
+        String str = "one"+"two"+12;
+        System.out.println(str);
+        
+        // for(int i =17 ; i<=170;i=i+17){
+        //     System.out.println(i);
+        // }
+        
+        // int table =17;
+        // for(int i =1; i<=10;i=i+1){
+        //     String str= table + " * "+i+" = "+table*i;
+        //     System.out.println(str);
+            
+        // }
+
+        // 2 table => 
+        // 2,4,6,8,10.12.14,16,18,20
+        // 3 table 
+
+
+        // 2*1=2
+        // 2*2=4
+        // ...
+        // 2*10=20
+
+        
+        
+        
+        // for(int i=0;i<5;i=i+1){
+        //     System.out.println("enter the first number");
+        //     int a=sc.nextInt();
+        //     System.out.println("Enter the second number");
+        //     int b=sc.nextInt();
+        //     int sum = a+b;
+        //     System.out.println("the sum is : " +sum);
+        // }
+
+
 
 
 
