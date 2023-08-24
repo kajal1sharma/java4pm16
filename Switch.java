26019bf5f933ae96c857d830880c3fd6b0a779df
kajal <kajal@gmail.com>
2023-08-24 16:27:14 +0530

    ..

--- Switch.java
+++ Switch.java
@@ -15,21 +15,21 @@ public class Switch {
         int day= sc.nextInt();
 
 
-        switch(day){
+        switch(day-1+2){
             case 1:System.out.println("Monday");
-            
+                    break;
             case 2:System.out.println("Tuesday");
-                
+                    break;
             case 3:System.out.println("Wednesday");
-                
+                    break;
             case 4:System.out.println("Thursday");
-                   
+                   break;
             case 5:System.out.println("Friday");
-                    
+                    break;
             case 6:System.out.println("Saturday");
-                    
+                    break;
             case 7:System.out.println("Sunday");
-                    
+                    break;
             default:System.out.println("Not a valid day");
         }
 
