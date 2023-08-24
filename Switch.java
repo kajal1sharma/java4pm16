5262f3f46c948f9a00467fe6ad2ad1dd853f484d
kajal <kajal@gmail.com>
2023-08-24 16:47:43 +0530

    ..

--- Switch.java
+++ Switch.java
@@ -12,26 +12,61 @@ public class Switch {
         //loop logic 
 
         Scanner sc = new Scanner(System.in);
-        int day= sc.nextInt();
+        // int day= sc.nextInt();
 
 
-        switch(day-1+2){
-            case 1:System.out.println("Monday");
-                    break;
-            case 2:System.out.println("Tuesday");
-                    break;
-            case 3:System.out.println("Wednesday");
-                    break;
-            case 4:System.out.println("Thursday");
-                   break;
-            case 5:System.out.println("Friday");
-                    break;
-            case 6:System.out.println("Saturday");
-                    break;
-            case 7:System.out.println("Sunday");
-                    break;
-            default:System.out.println("Not a valid day");
+
+        //calculator
+
+        //+,-,*,/
+
+        System.out.println("user give some input => +,-,*,/");
+        char ch = sc.next().charAt(0);
+
+        int a, b;
+        System.out.println("Enter first number");
+        a=sc.nextInt();
+        System.out.println("Enter the second number");
+        b=sc.nextInt();
+        
+        switch(ch){
+            case '+':
+                     int sum = a+b;
+                     System.out.println(sum);
+                     break;
+            case '-':
+                     int diff = a-b;
+                     System.out.println(diff);
+                     break;
+            case '/':
+                     int div = a/b;
+                     System.out.println(div);
+                     break;
+
+            case '*':
+                     int mul = a*b;
+                     System.out.println(mul);
+                     break;
+            default :System.out.println("Wrong input");
         }
+
+        // switch(day-1+2){
+        //     case 1:System.out.println("Monday");
+        //             break;
+        //     case 2:System.out.println("Tuesday");
+        //             break;
+        //     case 3:System.out.println("Wednesday");
+        //             break;
+        //     case 4:System.out.println("Thursday");
+        //            break;
+        //     case 5:System.out.println("Friday");
+        //             break;
+        //     case 6:System.out.println("Saturday");
+        //             break;
+        //     case 7:System.out.println("Sunday");
+        //             break;
+        //     default:System.out.println("Not a valid day");
+        // }
 
         // if(day==1){
         //     System.out.println("Monday");
