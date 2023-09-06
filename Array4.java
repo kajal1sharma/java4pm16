b24805f511bdcc7ce159c16526a36a0eae835f34
kajal <kajal@gmail.com>
2023-09-06 17:08:06 +0530

    ..

--- Array4.java
+++ Array4.java
@@ -19,19 +19,51 @@ public class Array4 {
             }
         }
 
-        //logic
-        for(int i=0;i<size;i=i+1){
-            
-            for(int j=0;j<size;j=j+1){
-                if(i<=j){
-                    System.out.print(arr[i][j]+" ");
-                }
-                else{
-                    System.out.print("0 " );                    
-                }
-            }
-            System.out.println();
+        int i=0;
+        int j=0;
+        int lrsum=0;
+        while(i<size){
+            lrsum =lrsum + arr[i][j];
+            i=i+1;
+            j=j+1;
         }
+        // i=0;
+        // j=size-1;
+        int rlsum=0;
+        
+        for(int k=0,l=size-1;k<size;k=k+1,l=l-1){
+            rlsum=rlsum+arr[i][j];
+        }
+
+        // while(i<size){
+        //     rlsum=rlsum+arr[i][j];
+        //     i=i+1;
+        //     j=j-1;
+
+        // }
+
+        
+        int diff = lrsum-rlsum;
+        diff =Math.abs(diff);
+        // if(diff<0){
+        //     diff=diff*-1;
+        // }
+        System.out.println(diff);
+
+
+        //logic
+        // for(int i=0;i<size;i=i+1){
+            
+        //     for(int j=0;j<size;j=j+1){
+        //         if(i<=j){
+        //             System.out.print(arr[i][j]+" ");
+        //         }
+        //         else{
+        //             System.out.print("0 " );                    
+        //         }
+        //     }
+        //     System.out.println();
+        // }
 
     }
 }
