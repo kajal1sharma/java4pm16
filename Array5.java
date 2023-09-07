2a428a0491c01e31d7850a937bddd4547517980f
kajal <kajal@gmail.com>
2023-09-07 17:26:23 +0530

    ..

---
+++ Array5.java
@@ -0,0 +1,45 @@
+public class Array5 {
+    public static void main(String[] args) {
+       //Q1 sort 
+       // int arr[] ={1,4,2,6,8,5,3,6}
+
+       
+       
+       
+        //Q2: 
+        //if size of the array is 5 than 1-6 
+        //print the missing number
+        // int arr[]={1,2,4,5,6};
+        // int brr[]={1,2,3,4,5,7,8,9};
+        // int brr[]={2,1,4,3,7,9,8,5};//size of array is  8 ,missing number is  6, (1-9)
+        
+
+        // boolean flag=false;
+        // for(int i=1;i<=brr.length+1;i=i+1){
+        //     flag=false;
+        //     for(int j=0;j<brr.length;j=j+1){
+        //         if(brr[j]==i){
+        //             flag=true;
+        //             break;
+        //         }
+        //     }
+        //     if(flag==true){
+        //         continue;
+        //     }
+        //     else{
+        //         System.out.println(i);
+        //         break;
+        //     }
+        // }
+
+        // for(int i=0;i< arr.length;i=i+1){
+        //     if((i+1)==arr[i]){
+        //         continue;
+        //     }
+        //     else{
+        //         System.out.println("missing element ="+(i+1));
+        //         break;
+        //     }
+        // }
+    }
+}
