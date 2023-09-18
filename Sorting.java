013aae2981ca070ec7e4d1cc79fc13fdcbcf0f11
kajal <kajal@gmail.com>
2023-09-18 16:37:10 +0530

    ..

--- Sorting.java
+++ Sorting.java
@@ -1,10 +1,8 @@
 class Sorting{
-    public static void main(String []args){
-        
-        int []arr = {9,7,6,5,4,3,2,1};//declaration and initialization at same time
 
-        //total number of cycles
-        for(int i=0;i< arr.length-1;i=i+1){
+
+    static void sort(int arr[]){
+    for(int i=0;i< arr.length-1;i=i+1){
             //total number of comparision
             for(int j=0;j<arr.length-1-i;j=j+1){
                 if(arr[j]>arr[j+1]){
@@ -14,11 +12,32 @@ class Sorting{
                 }
             }
         }
+    }
+    public static void main(String []args){
+        
+        int brr [] = {1,2,3,12,5,65,6,6,6};
+        sort(brr);
 
 
-        for(int i=0;i<arr.length;i=i+1){
-            System.out.println(arr[i]);
-        }
+
+        // int []arr = {9,7,6,5,4,3,2,1};//declaration and initialization at same time
+
+        // //total number of cycles
+        // for(int i=0;i< arr.length-1;i=i+1){
+        //     //total number of comparision
+        //     for(int j=0;j<arr.length-1-i;j=j+1){
+        //         if(arr[j]>arr[j+1]){
+        //             int temp=arr[j];
+        //             arr[j]=arr[j+1];
+        //             arr[j+1]=temp;
+        //         }
+        //     }
+        // }
+
+
+        // for(int i=0;i<arr.length;i=i+1){
+        //     System.out.println(arr[i]);
+        // }
 
 
     }
