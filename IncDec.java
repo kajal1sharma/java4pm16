2cfaa21ee3d83dc5766939bef13d390fdb4e55fe
kajal <kajal@gmail.com>
2023-09-19 15:34:26 +0530

    ..

---
+++ IncDec.java
@@ -0,0 +1,33 @@
+public class IncDec {
+
+    static int read(){
+        return 27;
+    }
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+          //  i++;
+        }
+
+        // int a=10;
+        // p=++a;
+        // int p=a;
+        System.out.println("==============================================");
+        for(int j=0;j<5;++j){
+            System.out.println(j);
+          //  ++j;
+        }
+        // int a=10;
+        // int p=++a;
+
+    //     //a=a+1
+    //    System.out.println(a);
+    //    System.out.println(p);
+
+        //a++ (post increment) 
+        // ++a;//a=a+1(pre incrememnt)
+        // --a;//a=a-1
+        // System.out.println(exp);
+        // System.out.println(a);
+    }
+}
