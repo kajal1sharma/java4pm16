94a3124b208d4b80a2ad9c1bd3798533a7066650
kajal <kajal@gmail.com>
2023-09-21 15:39:02 +0530

    ..

--- ZeroesOne.java
+++ ZeroesOne.java
@@ -1,26 +1,53 @@
 public class ZeroesOne {
     public static void main(String[] args) {
-        int arr[]={0,1,0,1,0,0,0,1,1,1};
+        int arr[]={0,0,1,1,1,0,1,0,0,0,1,1,1};
 
-        int countOfZeroes =0;
-        for(int i=0;i<arr.length ;i++){
-            if(arr[i]==0){
-                countOfZeroes++;
+
+        int i=0;
+        int j=arr.length-1;
+
+        for(;;){
+
+            for(;arr[i]==0;i++){
+                
             }
+
+            while(arr[j]==1){
+                j--;
+            }
+
+            if(j<i){
+                break;
+            }
+
+            int temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+
+        }
+        for(int k=0;k<arr.length;k++){
+            System.out.println(arr[k]);
         }
 
+        // int countOfZeroes =0;
+        // for(int i=0;i<arr.length ;i++){
+        //     if(arr[i]==0){
+        //         countOfZeroes++;
+        //     }
+        // }
 
-        for(int i=0;i<countOfZeroes;i++){
-            arr[i]=0;
-        }
-        for(int i=countOfZeroes;i<arr.length;i++){
-            arr[i]=1;
-        }
+
+        // for(int i=0;i<countOfZeroes;i++){
+        //     arr[i]=0;
+        // }
+        // for(int i=countOfZeroes;i<arr.length;i++){
+        //     arr[i]=1;
+        // }
 
 
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.println(arr[i]);
+        // }
 
 
     }
