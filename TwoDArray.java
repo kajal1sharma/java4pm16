19616d010347f8ac4a82dfdb6cdceaeb04c7c648
kajal <kajal@gmail.com>
2023-10-04 15:14:34 +0530

    ..

--- TwoDArray.java
+++ TwoDArray.java
@@ -4,15 +4,17 @@ public class TwoDArray {
         // int [][] arr;
         // arr= new int[4][3];//number of rows, colms
     
-        int arr[][]={{1,2,3,7},{3,2,4},{5,7,2,4,2},{4,5}};
-        
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        
-        }
+        int arr[][]={{1,2,3,7},{3,2,4,3},{5,7,2,2},{4,5,4,5}};
+       
+     //    for(int i=0;i<arr.length;i++){
+     //         int sum=0;
+     //        for(int j=0;j<arr[i].length;j++){
+     //            // System.out.print(arr[i][j]+" ");
+     //            sum=sum+arr[j][i];
+     //        }
+     //         System.out.println(sum);
+     //    }
+       
 
         //app2=> same line initialization
         //int brr[][] = new int[2][2];
