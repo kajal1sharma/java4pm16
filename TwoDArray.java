34f411638c0c87dc901f9cd319a03e313a7e8f93
kajal <kajal@gmail.com>
2023-10-04 16:25:51 +0530

    ..

--- TwoDArray.java
+++ TwoDArray.java
@@ -2,16 +2,28 @@ public class TwoDArray {
      public static void main(String[]args){
         
         
-          int [][]arr={{1,2,3},{3,4,5},{3,2,5}};
+
+          int [][][] arr = {{{1,2,3},{2,3,4},{4,5,6}},{{1,2,3},{7,8,9},{5,6,7}},{{2,3,4},{4,5,6},{3,4,5}}};
+
+          // int arr[][] = new int[4][];
 
 
-          for(int j=0;j<arr[0].length;j++){
+          // arr[0]= new int[12];
+          // arr[1]=new int[4];
+          // arr[2]=new int[8];
+          // arr[3]=new int[4];
 
-               for(int i=0;i<arr.length;i++){
-                    System.out.print(arr[i][j]+" ");
-               }
-               System.out.println();
-          }
+          // int [][]arr={{1,2,3,9,0},{3,4,5,1},{3,2,5}};
+
+          // int arr2[][]=new int[2][3];
+
+          // for(int j=0;j<arr[0].length;j++){
+
+          //      for(int i=0;i<arr.length;i++){
+          //           System.out.print(arr[i][j]+" ");
+          //      }
+          //      System.out.println();
+          // }
 
           // for(int i=0;i<arr.length;i++){
           //      for(int j=0;j<arr[i].length;j++){
