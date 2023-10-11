1931b64bd3790063573b5a7882e7681e58f409c6
kajal <kajal@gmail.com>
2023-10-11 16:17:05 +0530

    ..

---
+++ oops/CountStudent.java
@@ -0,0 +1,39 @@
+
+class Student3{
+    static int count;
+    int rollno;
+    String name;
+
+    int incrementCount(){
+        count++;
+        return count;
+    }
+}
+
+public class CountStudent {
+    public static void main(String[] args) {
+
+        Student3[] arr =new Student3[5];
+
+        for(int i=0;i<5;i++){
+            arr[i]=new Student3();
+        }
+
+        // Student3 obj1=new Student3();
+        // obj1.incrementCount();
+        // Student3 obj2=new Student3();
+        // obj2.incrementCount();
+        // Student3 obj3=new Student3();
+        // obj3.incrementCount();
+       
+        // Student3 obj4=new Student3();
+        // obj4.incrementCount();
+
+
+        //  System.out.println(obj1.count);
+        //  System.out.println(obj2.count);
+        //  System.out.println(obj3.count);
+        // System.out.println(obj4.count);
+       // System.out.println("total student : "+Student3.count);
+    }
+}
--- oops/StaticKey.java
+++ oops/StaticKey.java
@@ -45,17 +45,19 @@ public class StaticKey {
     public static void main(String[] args) {
         Student1.principal="vk singh";
         Student1 obj1= new Student1();
+        obj1.principal="ln mishra";
         obj1.setPrincipal("sk shrma");
         obj1.setName("meena");
 
         //static variables can be accessed through classname
         //there is only one copy created for static variables
         //that resides in method area with the class def
-        
+
         Student1.principal="mn gandhi";
 
         Student1 obj2 =new Student1();
         String principalName = obj2.getPrincipal();
+        principalName= obj2.principal;
         System.out.println(principalName);
         // Student1 obj2 = new Student1();
         // obj2.setPrincipal("vk singh");
