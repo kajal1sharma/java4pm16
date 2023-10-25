ede43c2d505fbdf77be8e2ea6de5bed5b597ba40
kajal <kajal@gmail.com>
2023-10-25 16:11:41 +0530

    ..

---
+++ oops/Overriding.java
@@ -0,0 +1,34 @@
+class Base{
+    int x;
+    Base(int x){
+        this.x=x;
+    }
+    void getInfo(){
+        System.out.println("iam getInfo function of base class");
+        System.out.println(x);
+    }
+}
+
+class Child2 extends Base{
+    int y;
+    Child2(int x, int y){
+        super(x);
+        this.y=y;
+    }
+    void getInfo(){
+        System.out.println("iam getInfo function of child class");
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+}
+
+public class Overriding {
+    public static void main(String[] args) {
+        Base b= new Base(10);
+        b.getInfo();
+
+        Child2 c= new Child2(20,30);
+        c.getInfo();
+    }    
+}
