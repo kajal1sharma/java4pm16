5f868a3a4fdda909b134bfc1e243bbee460e2816
kajal <kajal@gmail.com>
2023-11-09 15:26:15 +0530

    ..

---
+++ oops/Finalfunction.java
@@ -0,0 +1,34 @@
+
+
+//abstract class / inteface 
+
+// final interface dog{
+
+// }
+
+// final abstract class dog{
+
+// }
+
+final class Mobile{
+    final void printInfo(){
+        System.out.println("Iam a mobile class function");
+    }
+
+    // void printInfo(int a){
+    //     System.out.println("iam some other function");
+    // }
+}
+
+//you cannot extends final class
+// public class Finalfunction extends Mobile {
+
+// }
+// public class Finalfunction  extends Mobile{
+
+//     //overriding of finjal methods cannot be done
+//     // void printInfo(){
+//     //     System.out.println("some other definition");
+//     // }
+    
+// }
---
+++ oops/Revision.java
@@ -0,0 +1,70 @@
+//classes and object
+
+class Phone{
+    private int model;
+    private String color;
+    private int ram;
+    private int camera;
+    private int cost;
+
+    Phone(){
+
+    }
+    public Phone(int model, String color, int ram, int camera, int cost){
+        this.model=model;
+        this.color=color;
+        this.ram=ram;
+        this.camera=camera;
+        this.cost=cost;
+    }
+    public void setCamera(int camera) {
+        this.camera = camera;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+    public void setModel(int model) {
+        this.model = model;
+    }
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+    public int getCamera() {
+        return camera;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getCost() {
+        return cost;
+    }
+    public int getModel() {
+        return model;
+    }
+    public int getRam() {
+        return ram;
+    }
+
+
+}
+
+class Phone2 extends Phone{
+    Phone2(int model, String color, int ram, int camera, int cost){
+         super(model, color, ram, camera,cost);
+        // this.color=color;
+    }
+}
+
+public class Revision {
+    public static void main(String[] args) {
+        Phone p1= new Phone(123, "white", 4, 16, 8000);    
+        Phone p2= new Phone(128, "white", 4, 16, 8000); 
+        Phone p3= new Phone(178, "golden", 16, 32, 18000);         
+        Phone2 p4 = new Phone2(156,"white", 86, 67, 90000);
+
+        System.out.println(p4.getCamera());
+    }    
+}
